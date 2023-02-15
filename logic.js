5ee5672eb0949225451930c33647f100f1ba276a
shubhamsc231 <46850914+shubhamsc231@users.noreply.github.com>
2023-02-15 16:01:49 +0530

    Added changes for Calculator

--- logic.js
+++ logic.js
@@ -1,5 +1,25 @@
-var i=1;
-a();
-function a(){
-    console.log("aa");
-};
+let screen = document.getElementById('output');
+buttons = document.querySelectorAll('button');
+let screenValue = '';
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        if (buttonText == 'X') {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if (buttonText == 'C') {
+            screenValue = "";
+            screen.value = screenValue;
+        }
+        else if (buttonText == '=') {
+            screen.value = eval(screenValue);
+        }
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+    })
+}
